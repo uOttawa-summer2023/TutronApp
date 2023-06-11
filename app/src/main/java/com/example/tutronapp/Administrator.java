449d0a869387c301e41be63c6509ba9588617843
hnezh063 <65682982+hnezh063@users.noreply.github.com>
2023-06-11 18:08:09 -0400

    Update Administrator.java

--- app/src/main/java/com/example/tutronapp/Administrator.java
+++ app/src/main/java/com/example/tutronapp/Administrator.java
@@ -9,6 +9,6 @@ public class Administrator extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student);
+        setContentView(R.layout.activity_Administrator);
     }
 }
